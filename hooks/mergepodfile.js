fe8cc04bf98988e60e585f10f013f87792711e15
itzmohd <56501550+itzmohd@users.noreply.github.com>
2023-10-24 22:43:26 +0300

    Update mergepodfile.js

--- hooks/mergepodfile.js
+++ hooks/mergepodfile.js
@@ -13,16 +13,6 @@ module.exports = function (ctx) {
         projectPodfile,
         path.join(ctx.opts.plugin.dir, "src", "ios", "Podfile")
     ];
-
-    // Read the Podfile
-    const podfileContent = fs.readFileSync(projectPodfile, 'utf8');
-    // Replace specific pod version for OneSignal
-    const updatedPodfileContent = podfileContent.replace(/pod 'OneSignal', '~> \d+\.\d+\.\d+'/g, "pod 'OneSignal', '~> 2.16.7'");
-
-    // Write back the updated Podfile content
-    fs.writeFileSync(projectPodfile, updatedPodfileContent, 'utf8');
-
-    console.log("Files writeFileSync successfully");
     
     mergeFiles(inputPathList, outputPath).then((status) => {
         if (status){
