6258e2f9ee8efa5a3f031afd2d0335aeeb4aba82
itzmohd <56501550+itzmohd@users.noreply.github.com>
2023-10-24 21:23:29 +0300

    Update mergepodfile.js

--- hooks/mergepodfile.js
+++ hooks/mergepodfile.js
@@ -15,28 +15,19 @@ module.exports = function (ctx) {
     ];
 
     mergeFiles(inputPathList, outputPath).then((status) => {
-        if (status) {
+        if (status){
             console.log("Files merged successfully");
-
-            // Read the merged Podfile
-            var mergedPodfileContent = fs.readFileSync(outputPath, 'utf8');
-
-            // Replace specific pod version for OneSignal
-            var updatedPodfileContent = mergedPodfileContent.replace(/pod 'OneSignal', '~> \d+\.\d+\.\d+'/g, "pod 'OneSignal', '~> 2.16.7'");
-
-            // Write back the updated Podfile content
-            fs.writeFileSync(outputPath, updatedPodfileContent, 'utf8');
-
-            // Remove the old Podfile and copy the updated one
-            fs.unlinkSync(projectPodfile);
-            fs.copyFileSync(outputPath, projectPodfile, fs.constants.COPYFILE_FICLONE, function (err) {
-                if (err) {
-                    throw (err);
-                }
-                console.log("Ended merging pod files to add the post_install script");
-            });
         } else {
             throw ("Error merging files");
         }
+
+        //remove the old Podfile (To make it compatible with MABS)
+        fs.unlinkSync(projectPodfile);
+        fs.copyFileSync(outputPath, projectPodfile, fs.constants.COPYFILE_FICLONE, function(err){
+            if (err){
+                throw (err);
+            }
+            console.log("Ended merging pod files to add the post_install script");
+        });
     });
 }
