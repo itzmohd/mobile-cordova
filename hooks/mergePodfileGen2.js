c2170a88cd73d41faaa15a8750ca53227f1683c0
itzmohd <56501550+itzmohd@users.noreply.github.com>
2024-07-22 10:16:36 +0300

    Update mergePodfileGen2.js

--- hooks/mergePodfileGen2.js
+++ hooks/mergePodfileGen2.js
@@ -4,29 +4,28 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = function(context) {
-        const platforms = context.opts.cordova.platforms;
+    const platforms = context.opts.cordova.platforms;
 
-        if (platforms.indexOf('ios') === -1) {
-            return;
-        }
+    if (platforms.indexOf('ios') === -1) {
+        return;
+    }
 
-        const iosPlatformPath = path.join(context.opts.projectRoot, 'platforms', 'ios');
-        const podfilePath = path.join(iosPlatformPath, 'Podfile');
-        var rootdir = ctx.opts.projectRoot;
-        var projectPodfile = path.join(rootdir, "platforms", "ios", "Podfile");
+    const iosPlatformPath = path.join(context.opts.projectRoot, 'platforms', 'ios');
+    const podfilePath = path.join(iosPlatformPath, 'Podfile');
+    const hookScriptPath = path.join(context.opts.projectRoot, 'hooks', 'postInstallHook.rb');
 
-        if (fs.existsSync(podfilePath)) {
-            let podfileContent = fs.readFileSync(podfilePath, 'utf8');
+    if (fs.existsSync(podfilePath) && fs.existsSync(hookScriptPath)) {
+        let podfileContent = fs.readFileSync(podfilePath, 'utf8');
+        const hookScript = fs.readFileSync(hookScriptPath, 'utf8');
 
-            let hookScript = fs.readFileSync(projectPodfile, 'utf8');
-
-            if (!podfileContent.includes("post_install do |installer|")) {
-                podfileContent += hookScript;
-                fs.writeFileSync(podfilePath, podfileContent, 'utf8');
-                console.log('Added post_install hook to Podfile');
-            } else {
-                console.log('post_install hook already exists in Podfile');
-            }
+        if (!podfileContent.includes("post_install do |installer|")) {
+            podfileContent += '\n' + hookScript;
+            fs.writeFileSync(podfilePath, podfileContent, 'utf8');
+            console.log('Added post_install hook to Podfile');
         } else {
-            console.log('Podfile not found.');
+            console.log('post_install hook already exists in Podfile');
         }
+    } else {
+        console.log('Podfile or postInstallHook.rb not found.');
+    }
+};
